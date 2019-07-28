0b8dcfcd60efa4ce8c9e2548649a6b506ba5d007
Robert Zuniga <github@mastertheday.com>
2019-07-28 18:53:04 -0400

    getting dependencies in check

--- liri.js
+++ liri.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 var keys = require("./keys.js");
-// var spotify = new Spotify(keys.spotify);
+var Spotify = require('node-spotify-api')
+//var spotify = new Spotify(keys.spotify);
 
 var fs = require("fs");
 
