fc1b2e6a048516bbc0c3bec653701064ba3d93a4
Robert Zuniga <github@mastertheday.com>
2019-07-28 18:31:31 -0400

    adding npm libraries

--- liri.js
+++ liri.js
@@ -1,4 +1,3 @@
-
 require("dotenv").config();
 var keys = require("./keys.js");
 // var spotify = new Spotify(keys.spotify);
@@ -8,12 +7,23 @@ var fs = require("fs");
 var myLiriRequest = process.argv[2];
 
 
-switch(myLiriRequest) {
-    case "concert-this": console.log("concert-this ok"); break;
-    case "spotify-this-song" : console.log("spotify-this-song ok"); break;
-    case "movie-this" : console.log("movie-this ok"); break;
-    case "do-what-it-says" : console.log("do-what-it-says ok"); break;
-    default: console.log("default case ok"); 
+switch (myLiriRequest) {
+    case "concert-this":
+        console.log("concert-this ok");
+        // http://rest.bankdsintown.com/
+        break;
+    case "spotify-this-song":
+        console.log("spotify-this-song ok");
+        break;
+    case "movie-this":
+        console.log("movie-this ok");        
+        // http://www.omdbapi.com/?apikey=[yourkey]&
+        // https://www.omdbapi.com/
+
+        break;
+    case "do-what-it-says":
+        console.log("do-what-it-says ok");
+        break;
+    default:
+        console.log("default case ok");
 }
-
-
